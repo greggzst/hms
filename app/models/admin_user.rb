33c88dc4b2aba53681a3ef724dffeb826a8c8aa0
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-12-14 15:42:27 +0100

    Configure admin login and add slim

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,12 +1,14 @@
 class AdminUser < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :rememberable,
          :validatable,
          :authentication_keys => [:username]
 
   def will_save_change_to_email?
+    false
+  end
+
+  def email_required?
     false
   end
 end
