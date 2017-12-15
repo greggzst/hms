0a49b6daf0b6dc6483bf5823c634d704e45e2454
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-12-15 14:23:12 +0100

    Add room partial and query method

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,7 @@
 class RoomsController < ApplicationController
   def index
     @rooms_filter_form = RoomsFilterForm.new(start_date: Date.today, end_date: (Date.today + 1.day), number_of_guests: 2)
+    @rooms = @rooms_filter_form.query
   end
 
   def filter
--- app/forms/rooms_filter_form.rb
+++ app/forms/rooms_filter_form.rb
@@ -2,4 +2,16 @@ class RoomsFilterForm
   include ActiveModel::Model
 
   attr_accessor :start_date, :end_date, :number_of_guests
+
+  def query
+    reservations_rooms = Reservation.where('date(start_date) >= ? AND date(end_date) <= ?', start_date, end_date).includes(:reservation_rooms, :rooms).map(&:reservation_rooms)
+    if reservations_rooms.any?
+      booked_rooms_ids = reservations_rooms.map do |rr|
+        rr.room_id if rr.amount_reserved == rr.room.room_amount
+      end
+      Room.where('id NOT IN (?) AND capacity = ?', booked_rooms_ids, number_of_guests)
+    else
+      Room.where(capacity: number_of_guests)
+    end
+  end
 end
