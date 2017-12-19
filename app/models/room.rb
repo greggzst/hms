eea0fca2c74ea4e5758548de3d6ecd9b31e95a58
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-12-19 12:04:48 +0100

    Fix for no photo

--- app/models/room.rb
+++ app/models/room.rb
@@ -7,8 +7,13 @@ class Room < ApplicationRecord
   validates :name, :capacity, :price, :room_amount, presence: true
   accepts_nested_attributes_for :photos, allow_destroy: true
 
-  def primary_photo_url(version)
-    photos.where(is_primary: true).first.image_url(version)
+  def primary_photo_url(version = nil)
+    if primary = primary_photo
+      "#{(version ? primary.image_url(version) : primary.image_url)}"
+    else
+      uploader = ImageUploader.new
+      version ? uploader.send(version).url : uploader.url
+    end
   end
 
   def primary_photo
