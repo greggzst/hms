17dd3140f2afa9b4c34d40e1588a8717160a17fd
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-12-19 18:26:32 +0100

    Fix query for same dates

--- app/forms/rooms_filter_form.rb
+++ app/forms/rooms_filter_form.rb
@@ -9,22 +9,26 @@ class RoomsFilterForm
   end
 
   def query
-    reservations_rooms = Reservation.where(
-      "is_cancelled = ? AND (date(start_date) < ? AND date(end_date) > ?)",
-      false,
-      end_date,
-      start_date
-      ).includes(
-        :reservation_rooms,
-        :rooms).map(&:reservation_rooms).flatten.group_by{|rr| [rr.room_id, rr.room]}
+    if start_date != end_date
+      reservations_rooms = Reservation.where(
+        "is_cancelled = ? AND (date(start_date) < ? AND date(end_date) > ?)",
+        false,
+        end_date,
+        start_date
+        ).includes(
+          :reservation_rooms,
+          :rooms).map(&:reservation_rooms).flatten.group_by{|rr| [rr.room_id, rr.room]}
 
-    if reservations_rooms.any?
-      booked_rooms_ids = reservations_rooms.map do |key, value|
-        key[0] if value.count == key[1].room_amount
-      end.uniq.join(',')
-      Room.where('id NOT IN (?) AND capacity >= ?', booked_rooms_ids, number_of_guests)
+      if reservations_rooms.any?
+        booked_rooms_ids = reservations_rooms.map do |key, value|
+          key[0] if value.count == key[1].room_amount
+        end.uniq.join(',')
+        Room.where('id NOT IN (?) AND capacity >= ?', booked_rooms_ids, number_of_guests)
+      else
+        Room.where('capacity >= ?', number_of_guests)
+      end
     else
-      Room.where('capacity >= ?', number_of_guests)
+      []
     end
   end
 end
