b7d9acf6fe4eeebfca4580b09f671e19a1b6fbf5
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-12-30 16:25:51 +0100

    Implement room spec

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Validation' do
+    before(:each) do
+      @room = Room.new
+    end
+
+    it 'is valid with valid attributes' do
+      @room.name = 'test name'
+      @room.description = 'test description'
+      @room.capacity = 2
+      @room.price = 120
+      @room.room_amount = 1
+      expect(@room).to be_valid
+    end
+
+    it 'is invalid without name' do
+      @room.capacity = 2
+      @room.price = 120
+      @room.room_amount = 1
+      expect(@room).to_not be_valid
+    end
+
+    it 'is invalid without capacity' do
+      @room.name = 'test name'
+      @room.capacity = nil
+      @room.price = 120
+      @room.room_amount = 1
+      expect(@room).to_not be_valid
+    end
+
+    it 'is invalid without price' do
+      @room.name = 'test name'
+      @room.capacity = 2
+      @room.price = nil
+      @room.room_amount = 1
+      expect(@room).to_not be_valid
+    end
+
+    it 'is invalid without room_amount' do
+      @room.name = 'test name'
+      @room.capacity = 2
+      @room.price = 120
+      @room.room_amount = nil
+      expect(@room).to_not be_valid
+    end
+
+    it 'is invalid without all attributes' do
+      expect(@room).to_not be_valid
+    end
+  end
 end
