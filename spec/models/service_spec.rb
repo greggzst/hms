85d0974a41a7eac81e60dab70b6878dd1298f2b5
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-12-30 16:30:16 +0100

    Implement service spec

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Service, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Validation' do
+    before(:each) do
+      @service = Service.new
+    end
+
+    it 'is valid with valid attributes' do
+      @service.name = 'test name'
+      @service.price = 120
+      expect(@service).to be_valid
+    end
+
+    it 'is invalid without name' do
+      @service.price = 120
+      expect(@service).to_not be_valid
+    end
+
+    it 'is invalid without price' do
+      @service.name = 'test name'
+      @service.price = nil
+      expect(@service).to_not be_valid
+    end
+
+    it 'is invalid without all attributes' do
+      expect(@service).to_not be_valid
+    end
+  end
 end
